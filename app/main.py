2ec1cefa65ed218fa65a202577d32d5720939188
Santhosh Thirunagaru <thirunagarusanthosh@gmail.com>
2026-02-19 23:33:01 +0530

    feat: code for product is ready and linked with postgre SQL

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.database import engine
+from app.models import product
+from app.database import Base
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
---
+++ app/models/product.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.database import Base
+class Product(Base):
+    __tablename__ = "products"
+    id=Column(Integer, primary_key=True, index=True)
+    name= Column(String, nullable=False)
+    category= Column(String, nullable=False)
+    brand= Column(String)
+    size= Column(String)
+    color= Column(String)
+    purchase_price= Column(Float, nullable=False)
+    selling_price= Column(Float, nullable=False)
+    quantity_brought= Column(Integer, default=0)
+    quantity_sold= Column(Integer, default=0)
+    city= Column(String)
