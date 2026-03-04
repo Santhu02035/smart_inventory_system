22c8dce6a3f022674bbc9b71ab02d6baf02acc25
Santhosh Thirunagaru <thirunagarusanthosh@gmail.com>
2026-03-04 22:42:03 +0530

    feat: JWT Authentication

---
+++ app/routers/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import hash_password, verify_password, create_access_token
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
+
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.username == form_data.username).first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    access_token = create_access_token(data={"sub": user.username})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
